c17e5521f6233a8464037ce48f523b2568297408
Brianna Jordan <bryvjordan9@gmail.com>
2019-12-27 18:00:48 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,8 +7,8 @@ def roll_call_dwarves(dwarves)# code an argument here
      # Your code here
 end
 
-def summon_captain_planet?(planeteer_calls)# code an argument here
-  planeteer_calls.map do |x|
+def summon_captain_planet?(veggies)# code an argument here
+  veggies.map do |x|
     x.capitalize << "!"
   end
 end
